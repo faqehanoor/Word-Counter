a4ea5c1f3765451a932f0bdbf55a38f504edb495
Faqeha Noor <zaminalam20@gmail.com>
2024-04-17 07:20:57 -0700

    push my Word-Counter to github

---
+++ index.js
@@ -0,0 +1,16 @@
+//Import the 'inquirer' from the module:
+import inquirer from "inquirer";
+//Declare a constant 'answers' and assign it to the result of inquirer.prompt function
+const answer = await inquirer.prompt([
+    {
+        name: "Sentence",
+        type: "input",
+        message: "Enter your sentence to count the word: "
+    }
+]);
+const words = answer.Sentence.trim().split(" ");
+const myname = "Faqeha Noor";
+//Print array of words
+console.log(words);
+//Print the word count of the sentence to the console
+console.log(`Your sentence word count is ${words.length}`);
